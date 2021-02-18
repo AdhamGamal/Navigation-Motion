6655c27c1d5495169b82ba6385b50f60409c704b
AdhamGamal <adhammohamadgamal@yahoo.com>
2021-02-19 01:38:12 +0200

    - Update

--- app/src/main/java/com/amg/advanced/navigationmotion/MainActivity.kt
+++ app/src/main/java/com/amg/advanced/navigationmotion/MainActivity.kt
@@ -3,9 +3,6 @@ package com.amg.advanced.navigationmotion
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
-import android.widget.ImageView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -15,7 +12,6 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/amg/advanced/navigationmotion/ui/home/HomeFragment.kt
+++ app/src/main/java/com/amg/advanced/navigationmotion/ui/home/HomeFragment.kt
@@ -6,8 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.amg.advanced.navigationmotion.R
 
 class HomeFragment : Fragment() {
@@ -20,12 +19,12 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+                ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        })
+        }
         return root
     }
 }
